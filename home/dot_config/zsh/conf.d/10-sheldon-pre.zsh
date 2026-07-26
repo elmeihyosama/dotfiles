0e6e6e64b91dc4969ff3492fa7f2b3119d1b0b51
Osama Elmeihy <31680341-oelmeihy@users.noreply.gitlab.com>
2026-07-26 20:33:33 +0000

    Git ergonomics: forgit, config niceties, git-absorb

--- home/dot_config/zsh/conf.d/10-sheldon-pre.zsh
+++ home/dot_config/zsh/conf.d/10-sheldon-pre.zsh
@@ -3,3 +3,9 @@
 _zc="${XDG_DATA_HOME:-$HOME/.local/share}/sheldon/repos/github.com/zsh-users/zsh-completions/src"
 [[ -d "$_zc" ]] && fpath=("$_zc" $fpath)
 unset _zc
+
+# forgit reads FORGIT_NO_ALIASES at plugin-load time (guards its own `alias`
+# registration inside forgit.plugin.zsh), so this MUST be set before sheldon
+# loads plugins in 30-sheldon.zsh. See 47-forgit.zsh for the namespaced f*
+# aliases that replace forgit's defaults.
+export FORGIT_NO_ALIASES=1
